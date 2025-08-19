b25ffc57c7ed49429764f33de9205d19c875c2fc
Talha76 <mushfiqurrahman845@gmail.com>
2025-08-19 12:18:20 +0600

    Added custom k6 script with tenant

--- grafana/scripts/script.js
+++ grafana/scripts/script.js
@@ -4,15 +4,20 @@ import { sleep } from 'k6';
 export let options = {
   stages: [
     { target: 400, duration: '3s'  },
-    // { target: 400, duration: '27s' },
+    { target: 400, duration: '27s' },
     { target: 600, duration: '2s'  },
-    // { target: 600, duration: '18s' },
+    { target: 600, duration: '18s' },
     { target: 800, duration: '1s'  },
-    // { target: 800, duration: '9s' },
+    { target: 800, duration: '9s'  },
   ]
 };
 
 export default function () {
-  http.get('http://nginx/search');  // target your nginx service in docker-compose
+  const params = {
+    headers: {
+      "Host": "alpha.local",
+    }
+  }
+  http.get('http://nginx/search', params);  // target your nginx service in docker-compose
   sleep(1);
 }
